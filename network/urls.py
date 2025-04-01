faa71089a7157f6667786a7094832669010f165a
vlalef <vaz.alef@proton.me>
2025-04-01 19:09:39 -0300

    feat: adding the urls.py file with the url path for the endpoints

---
+++ network/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("explore", views.explore, name="explore"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("new_post", views.new_post, name="new_post"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("follow/<int:user_id>", views.follow, name="follow"),
+    path("unfollow/<int:user_id>", views.unfollow, name="unfollow"),
+    path("following", views.following, name="following"),
+    path("edit/<int:post_id>", views.edit_post, name="edit_post"),
+    path("like/<int:post_id>", views.like_post, name="like_post"),
+]
